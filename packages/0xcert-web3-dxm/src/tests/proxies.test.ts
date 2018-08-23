36525075433511eb5552c132e0ca3198a7d1f299
Tadej Vengust <vengust.tadej@gmail.com>
2018-08-23 13:17:23 +0200

    Add proxy test scaffold.

---
+++ packages/0xcert-web3-dxm/src/tests/proxies.test.ts
@@ -0,0 +1,13 @@
+import { Spec } from '@specron/spec';
+
+const spec = new Spec();
+
+spec.test('deploy minter with correct ERC20 transfer proxy address', async (ctx) => {
+  
+});
+
+spec.test('deploy minter with correct Xcert minter proxy address', async (ctx) => {
+  
+});
+
+export default spec;
