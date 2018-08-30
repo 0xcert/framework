b6f9bee9b4964edd7b7b066f0f67bf93e9385510
Tadej Vengust <vengust.tadej@gmail.com>
2018-08-30 13:23:23 +0200

    Add NFTokenMetadataEnumerable test mock.

---
+++ packages/0xcert-web3-erc721/src/tests/mocks/NFTokenMetadataEnumerableTestMock.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.4.24;
+
+import "../../contracts/mocks/NFTokenMetadataEnumerableMock.sol";
+
+contract NFTokenMetadataEnumerableTestMock is
+  NFTokenMetadataEnumerableMock
+{
+  constructor(
+    string _name,
+    string _symbol
+  )
+    NFTokenMetadataEnumerableMock(_name, _symbol)
+    public
+  {}
+
+  function checkUri(
+    uint256 _tokenId
+  )
+    external
+    view
+    returns (string)
+  {
+    return idToUri[_tokenId];
+  }
+
+  function ownerToIdsLen(
+    address _owner
+  )
+    external
+    view
+    returns (uint256)
+  {
+    return ownerToIds[_owner].length;
+  }
+
+  function ownerToIdbyIndex(
+    address _owner,
+    uint256 _index
+  )
+    external
+    view
+    returns (uint256)
+  {
+    return ownerToIds[_owner][_index];
+  }
+
+  function idToOwnerIndexWrapper(
+    uint256 _tokenId
+  )
+    external
+    view
+    returns (uint256)
+  {
+    return idToOwnerIndex[_tokenId];
+  }
+
+  function idToIndexWrapper(
+    uint256 _tokenId
+  )
+    external
+    view
+    returns (uint256)
+  {
+    return idToIndex[_tokenId];
+  }
+}
