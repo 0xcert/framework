94b239f0c67f71cf16607559bac165a3ad279679
Tadej Vengust <vengust.tadej@gmail.com>
2018-09-24 16:31:40 +0200

    Update selector with new contracts.

--- packages/0xcert-web3-xcert/src/contracts/utils/selector.sol
+++ packages/0xcert-web3-xcert/src/contracts/utils/selector.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.24;
 
 import "../xcert.sol";
+import "../burnable-xcert.sol";
+import "../revokable-xcert.sol";
+import "../mutable-xcert.sol";
 
 /**
  * @dev This contracts calculates interface id of Xcert contracts as described in EIP165:
@@ -22,9 +25,44 @@ contract Selector {
       i.mint.selector
       ^ i.conventionId.selector
       ^ i.tokenProof.selector
-      ^ i.updateTokenProof.selector
       ^ i.setAuthorizedAddress.selector
       ^ i.isAuthorizedAddress.selector
     );
+  }
+
+  /**
+   * @dev Calculates and returns interface ID for the burnable Xcert smart contract.
+   */
+  function calculateBurnableXcertSelector()
+    external
+    pure
+    returns (bytes4)
+  {
+    BurnableXcert i;
+    return i.burn.selector;
+  }
+
+  /**
+   * @dev Calculates and returns interface ID for the revokable Xcert smart contract.
+   */
+  function calculateRevokableXcertSelector()
+    external
+    pure
+    returns (bytes4)
+  {
+    RevokableXcert i;
+    return i.revoke.selector;
+  }
+
+  /**
+   * @dev Calculates and returns interface ID for the revokable Xcert smart contract.
+   */
+  function calculateMutableXcertSelector()
+    external
+    pure
+    returns (bytes4)
+  {
+    MutableXcert i;
+    return i.updateTokenProof.selector;
   }
 }
--- packages/0xcert-web3-xcert/src/tests/selector.test.ts
+++ packages/0xcert-web3-xcert/src/tests/selector.test.ts
@@ -38,3 +38,44 @@ spec.test('checks Xcert selector', async (ctx) => {
   const supports = await xcert.methods.supportsInterface(bytes).call();
   ctx.is(supports, true);
 });
+
+spec.test('checks mutable Xcert selector', async (ctx) => {
+  const selector = ctx.get('selector');
+  const xcert = await ctx.deploy({ 
+    src: './build/mutable-xcert-mock.json',
+    contract: 'MutableXcertMock',
+    args: ['Foo','F','0x9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658']
+  });
+
+  const bytes = await selector.methods.calculateMutableXcertSelector().call();
+  const supports = await xcert.methods.supportsInterface(bytes).call();
+  ctx.is(supports, true);
+});
+
+
+spec.test('checks burnable Xcert selector', async (ctx) => {
+  const selector = ctx.get('selector');
+  const xcert = await ctx.deploy({ 
+    src: './build/burnable-xcert-mock.json',
+    contract: 'BurnableXcertMock',
+    args: ['Foo','F','0x9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658']
+  });
+
+  const bytes = await selector.methods.calculateBurnableXcertSelector().call();
+  const supports = await xcert.methods.supportsInterface(bytes).call();
+  ctx.is(supports, true);
+});
+
+spec.test('checks revokable Xcert selector', async (ctx) => {
+  const selector = ctx.get('selector');
+  const xcert = await ctx.deploy({ 
+    src: './build/revokable-xcert-mock.json',
+    contract: 'RevokableXcertMock',
+    args: ['Foo','F','0x9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658']
+  });
+
+  const bytes = await selector.methods.calculateRevokableXcertSelector().call();
+  const supports = await xcert.methods.supportsInterface(bytes).call();
+  ctx.is(supports, true);
+});
+
