a3b01bca2443a6660395a40d5045b3a05f1aaa27
Tadej Vengust <vengust.tadej@gmail.com>
2018-09-25 13:52:17 +0200

    Add standard proxy interface.

---
+++ packages/0xcert-web3-proxy/src/contracts/iproxy.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @dev Standard interface for a dex proxy contract.
+ */
+interface Proxy {
+
+  /**
+   * @dev Executes an action.
+   * @param _target Target of execution.
+   * @param _a Address usually represention from.
+   * @param _b Address usually representing to.
+   * @param _c Integer usually repersenting amount/value/id.
+   */
+  function execute(
+    address _target,
+    address _a,
+    address _b,
+    uint256 _c
+  )
+    external;
+}
