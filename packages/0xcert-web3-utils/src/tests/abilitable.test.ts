eaec3a03114bd8aab102b9d8fb975eb6673e1918
Kristijan Sedlak <xpepermint@gmail.com>
2018-10-05 00:12:59 +0200

    Fix utils

--- packages/0xcert-web3-utils/src/tests/abilitable.test.ts
+++ packages/0xcert-web3-utils/src/tests/abilitable.test.ts
@@ -40,9 +40,9 @@ spec.test('check if sender address has zero ability', async (ctx) => {
   const abilitable = ctx.get('abilitable');
   const owner = ctx.get('owner');
   const bob = ctx.get('bob');
-  const ownerHasAbility0 = await abilitable.methods.isAble(owner, 0).call();
+  const ownerHasAbility0 = await abilitable.instance.methods.isAble(owner, 0).call();
   ctx.is(ownerHasAbility0, true);
-  const bobHasAbility0 = await abilitable.methods.isAble(bob, 0).call();
+  const bobHasAbility0 = await abilitable.instance.methods.isAble(bob, 0).call();
   ctx.is(bobHasAbility0, false);
 });
 
@@ -51,16 +51,16 @@ spec.test('successfuly assigns an ability', async (ctx) => {
   const owner = ctx.get('owner');
   const bob = ctx.get('bob');
 
-  let bobHasAbility1 = await abilitable.methods.isAble(bob, 1).call();
+  let bobHasAbility1 = await abilitable.instance.methods.isAble(bob, 1).call();
   ctx.is(bobHasAbility1, false);
-  await ctx.reverts(() => abilitable.methods.ability1().call({ from: bob }), '017001');
+  await ctx.reverts(() => abilitable.instance.methods.ability1().call({ from: bob }), '017001');
 
-  const logs = await abilitable.methods.assignAbilities(bob, [1]).send({ from: owner });
+  const logs = await abilitable.instance.methods.assignAbilities(bob, [1]).send({ from: owner });
   ctx.not(logs.events.AssignAbility, undefined);
 
-  bobHasAbility1 = await abilitable.methods.isAble(bob, 1).call();
+  bobHasAbility1 = await abilitable.instance.methods.isAble(bob, 1).call();
   ctx.is(bobHasAbility1, true);
-  await abilitable.methods.ability1().call({ from: bob });
+  await abilitable.instance.methods.ability1().call({ from: bob });
 });
 
 spec.test('successfuly assigns zero ability', async (ctx) => {
@@ -69,14 +69,14 @@ spec.test('successfuly assigns zero ability', async (ctx) => {
   const bob = ctx.get('bob');
   const jane = ctx.get('jane');
 
-  let bobHasAbility0 = await abilitable.methods.isAble(bob, 0).call();
+  let bobHasAbility0 = await abilitable.instance.methods.isAble(bob, 0).call();
   ctx.is(bobHasAbility0, false);
-  await ctx.reverts(() => abilitable.methods.assignAbilities(jane, [0]).send({ from: bob }), '017001');
+  await ctx.reverts(() => abilitable.instance.methods.assignAbilities(jane, [0]).send({ from: bob }), '017001');
 
-  const logs = await abilitable.methods.assignAbilities(bob, [0]).send({ from: owner });
+  const logs = await abilitable.instance.methods.assignAbilities(bob, [0]).send({ from: owner });
   ctx.not(logs.events.AssignAbility, undefined);
 
-  bobHasAbility0 = await abilitable.methods.isAble(bob, 0).call();
+  bobHasAbility0 = await abilitable.instance.methods.isAble(bob, 0).call();
   ctx.is(bobHasAbility0, true);
 });
 
@@ -85,30 +85,30 @@ spec.test('successfuly assigns multiple abilities', async (ctx) => {
   const owner = ctx.get('owner');
   const bob = ctx.get('bob');
 
-  let bobHasAbility0 = await abilitable.methods.isAble(bob, 0).call();
+  let bobHasAbility0 = await abilitable.instance.methods.isAble(bob, 0).call();
   ctx.is(bobHasAbility0, false);
-  let bobHasAbility1 = await abilitable.methods.isAble(bob, 1).call();
+  let bobHasAbility1 = await abilitable.instance.methods.isAble(bob, 1).call();
   ctx.is(bobHasAbility1, false);
-  let bobHasAbility2 = await abilitable.methods.isAble(bob, 2).call();
+  let bobHasAbility2 = await abilitable.instance.methods.isAble(bob, 2).call();
   ctx.is(bobHasAbility2, false);
-  let bobHasAbility60 = await abilitable.methods.isAble(bob, 60).call();
+  let bobHasAbility60 = await abilitable.instance.methods.isAble(bob, 60).call();
   ctx.is(bobHasAbility60, false);
-  await ctx.reverts(() => abilitable.methods.ability1().call({ from: bob }), '017001');
-  await ctx.reverts(() => abilitable.methods.ability2().call({ from: bob }), '017001');
+  await ctx.reverts(() => abilitable.instance.methods.ability1().call({ from: bob }), '017001');
+  await ctx.reverts(() => abilitable.instance.methods.ability2().call({ from: bob }), '017001');
 
-  const logs = await abilitable.methods.assignAbilities(bob, [0,1,2,60]).send({ from: owner });
+  const logs = await abilitable.instance.methods.assignAbilities(bob, [0,1,2,60]).send({ from: owner });
   ctx.is(logs.events.AssignAbility.length, 4);
 
-  bobHasAbility0 = await abilitable.methods.isAble(bob, 0).call();
+  bobHasAbility0 = await abilitable.instance.methods.isAble(bob, 0).call();
   ctx.is(bobHasAbility0, true);
-  bobHasAbility1 = await abilitable.methods.isAble(bob, 1).call();
+  bobHasAbility1 = await abilitable.instance.methods.isAble(bob, 1).call();
   ctx.is(bobHasAbility1, true);
-  bobHasAbility2 = await abilitable.methods.isAble(bob, 2).call();
+  bobHasAbility2 = await abilitable.instance.methods.isAble(bob, 2).call();
   ctx.is(bobHasAbility2, true);
-  bobHasAbility60 = await abilitable.methods.isAble(bob, 60).call();
+  bobHasAbility60 = await abilitable.instance.methods.isAble(bob, 60).call();
   ctx.is(bobHasAbility60, true);
-  await abilitable.methods.ability1().call({ from: bob });
-  await abilitable.methods.ability2().call({ from: bob });
+  await abilitable.instance.methods.ability1().call({ from: bob });
+  await abilitable.instance.methods.ability2().call({ from: bob });
 });
 
 spec.test('successfuly revokes an ability', async (ctx) => {
@@ -116,13 +116,13 @@ spec.test('successfuly revokes an ability', async (ctx) => {
   const owner = ctx.get('owner');
   const bob = ctx.get('bob');
 
-  await abilitable.methods.assignAbilities(bob, [1]).send({ from: owner });
-  const logs = await abilitable.methods.revokeAbilities(bob, [1]).send({ from: owner });
+  await abilitable.instance.methods.assignAbilities(bob, [1]).send({ from: owner });
+  const logs = await abilitable.instance.methods.revokeAbilities(bob, [1]).send({ from: owner });
   ctx.not(logs.events.RevokeAbility, undefined);
 
-  const bobHasAbility1 = await abilitable.methods.isAble(bob, 1).call();
+  const bobHasAbility1 = await abilitable.instance.methods.isAble(bob, 1).call();
   ctx.is(bobHasAbility1, false);
-  await ctx.reverts(() => abilitable.methods.ability1().call({ from: bob }), '017001');
+  await ctx.reverts(() => abilitable.instance.methods.ability1().call({ from: bob }), '017001');
 });
 
 spec.test('successfuly revokes zero ability', async (ctx) => {
@@ -131,13 +131,13 @@ spec.test('successfuly revokes zero ability', async (ctx) => {
   const bob = ctx.get('bob');
   const jane = ctx.get('jane');
 
-  await abilitable.methods.assignAbilities(bob, [0]).send({ from: owner });
-  const logs = await abilitable.methods.revokeAbilities(bob, [0]).send({ from: owner });
+  await abilitable.instance.methods.assignAbilities(bob, [0]).send({ from: owner });
+  const logs = await abilitable.instance.methods.revokeAbilities(bob, [0]).send({ from: owner });
   ctx.not(logs.events.RevokeAbility, undefined);
 
-  let bobHasAbility0 = await abilitable.methods.isAble(bob, 0).call();
+  let bobHasAbility0 = await abilitable.instance.methods.isAble(bob, 0).call();
   ctx.is(bobHasAbility0, false);
-  await ctx.reverts(() => abilitable.methods.assignAbilities(jane, [0]).send({ from: bob }), '017001');
+  await ctx.reverts(() => abilitable.instance.methods.assignAbilities(jane, [0]).send({ from: bob }), '017001');
 });
 
 spec.test('successfuly revokes multiple abilities', async (ctx) => {
@@ -145,32 +145,32 @@ spec.test('successfuly revokes multiple abilities', async (ctx) => {
   const owner = ctx.get('owner');
   const bob = ctx.get('bob');
 
-  await abilitable.methods.assignAbilities(bob, [0,1,2,60]).send({ from: owner });
-  const logs = await abilitable.methods.revokeAbilities(bob, [0,2,60]).send({ from: owner });
+  await abilitable.instance.methods.assignAbilities(bob, [0,1,2,60]).send({ from: owner });
+  const logs = await abilitable.instance.methods.revokeAbilities(bob, [0,2,60]).send({ from: owner });
   ctx.is(logs.events.RevokeAbility.length, 3);
 
-  const bobHasAbility0 = await abilitable.methods.isAble(bob, 0).call();
+  const bobHasAbility0 = await abilitable.instance.methods.isAble(bob, 0).call();
   ctx.is(bobHasAbility0, false);
-  const bobHasAbility1 = await abilitable.methods.isAble(bob, 1).call();
+  const bobHasAbility1 = await abilitable.instance.methods.isAble(bob, 1).call();
   ctx.is(bobHasAbility1, true);
-  const bobHasAbility2 = await abilitable.methods.isAble(bob, 2).call();
+  const bobHasAbility2 = await abilitable.instance.methods.isAble(bob, 2).call();
   ctx.is(bobHasAbility2, false);
-  const bobHasAbility60 = await abilitable.methods.isAble(bob, 60).call();
+  const bobHasAbility60 = await abilitable.instance.methods.isAble(bob, 60).call();
   ctx.is(bobHasAbility60, false);
-  await abilitable.methods.ability1().call({ from: bob });
-  await ctx.reverts(() => abilitable.methods.ability2().call({ from: bob }), '017001');
+  await abilitable.instance.methods.ability1().call({ from: bob });
+  await ctx.reverts(() => abilitable.instance.methods.ability2().call({ from: bob }), '017001');
 });
 
 spec.test('throws when trying to revoke zero ability when only one account has a zero ability', async (ctx) => {
   const abilitable = ctx.get('abilitable');
   const owner = ctx.get('owner');
 
-  await ctx.reverts(() => abilitable.methods.revokeAbilities(owner, [0]).send({ from: owner }), '017002');
+  await ctx.reverts(() => abilitable.instance.methods.revokeAbilities(owner, [0]).send({ from: owner }), '017002');
 });
 
 spec.test('throws when trying to assing ability array that overflows', async (ctx) => {
   const abilitable = ctx.get('abilitable');
   const owner = ctx.get('owner');
   const array = Array.from({ length: 256 }, (v, i) => i);
-  await ctx.throws(() => abilitable.methods.assignAbilities(owner, array).send({ from: owner }));
+  await ctx.throws(() => abilitable.instance.methods.assignAbilities(owner, array).send({ from: owner }));
 });
--- packages/0xcert-web3-utils/src/tests/safe-math.test.ts
+++ packages/0xcert-web3-utils/src/tests/safe-math.test.ts
@@ -28,8 +28,8 @@ spec.test('multiplies correctly', async (ctx) => {
   const safeMath = ctx.get('safeMath');
   const a = 5678;
   const b = 1234;
-  await safeMath.methods.multiply(a, b).send();
-  const result = await safeMath.methods.result().call();
+  await safeMath.instance.methods.multiply(a, b).send();
+  const result = await safeMath.instance.methods.result().call();
   ctx.is(result, (a * b).toString());
 });
 
@@ -37,8 +37,8 @@ spec.test('adds correctly', async (ctx) => {
   const safeMath = ctx.get('safeMath');
   const a = 5678;
   const b = 1234;
-  await safeMath.methods.add(a, b).send();
-  const result = await safeMath.methods.result().call();
+  await safeMath.instance.methods.add(a, b).send();
+  const result = await safeMath.instance.methods.result().call();
   ctx.is(result, (a + b).toString());
 });
 
@@ -46,8 +46,8 @@ spec.test('subtracts correctly', async (ctx) => {
   const safeMath = ctx.get('safeMath');
   const a = 5678;
   const b = 1234;
-  await safeMath.methods.subtract(a, b).send();
-  const result = await safeMath.methods.result().call();
+  await safeMath.instance.methods.subtract(a, b).send();
+  const result = await safeMath.instance.methods.result().call();
   ctx.is(result, (a - b).toString());
 });
 
@@ -55,19 +55,19 @@ spec.test('should throw an error if subtraction result would be negative', async
   const safeMath = ctx.get('safeMath');
   const a = 1234;
   const b = 5678;
-  await ctx.throws(() => safeMath.methods.subtract(a, b).send());
+  await ctx.throws(() => safeMath.instance.methods.subtract(a, b).send());
 });
 
 spec.test('should throw an error on addition overflow', async (ctx) => {
   const safeMath = ctx.get('safeMath');
   const a = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
   const b = 1;
-  await ctx.throws(() => safeMath.methods.add(a, b).send());
+  await ctx.throws(() => safeMath.instance.methods.add(a, b).send());
 });
 
 spec.test('should throw an error on multiplication overflow', async (ctx) => {
   const safeMath = ctx.get('safeMath');
   const a = 115792089237316195423570985008687907853269984665640564039457584007913129639933;
   const b = 2;
-  await ctx.throws(() => safeMath.methods.multiply(a, b).send());
+  await ctx.throws(() => safeMath.instance.methods.multiply(a, b).send());
 });
