c76bfb83197af5bfab6b94e42b6f9e9d75f0c8d5
Tadej Vengust <vengust.tadej@gmail.com>
2018-10-04 13:43:34 +0200

    Add supports interface.

---
+++ packages/0xcert-web3-utils/src/tests/supports-interface.test.ts
@@ -0,0 +1,37 @@
+import { Spec } from '@specron/spec';
+
+/**
+ * Spec context interfaces.
+ */
+
+interface Data {
+  supportsInterface?: any;
+}
+
+/**
+ * Spec stack instances.
+ */
+
+const spec = new Spec<Data>();
+
+export default spec;
+
+spec.beforeEach(async (ctx) => {
+  const supportsInterface = await ctx.deploy({ 
+    src: './build/supports-interface.json',
+    contract: 'SupportsInterface',
+  });
+  ctx.set('supportsInterface', supportsInterface);
+});
+
+spec.test('correctly checks all the supported interfaces', async (ctx) => {
+  const supportsInterface = ctx.get('supportsInterface');
+  const erc165 = await supportsInterface.methods.supportsInterface('0x01ffc9a7').call();
+  ctx.is(erc165, true);
+});
+
+spec.test('checks if 0xffffffff is false', async (ctx) => {
+  const supportsInterface = ctx.get('supportsInterface');
+  const erc165 = await supportsInterface.methods.supportsInterface('0xffffffff').call();
+  ctx.is(erc165, false);
+});
