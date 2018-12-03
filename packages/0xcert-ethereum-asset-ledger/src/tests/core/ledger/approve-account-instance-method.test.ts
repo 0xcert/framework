a5d52f9ebae50ed07a42953ef23042f716fb37c0
Kristijan Sedlak <xpepermint@gmail.com>
2018-12-03 21:49:27 +0100

    Support approve of order gateway on asset ledger

--- packages/0xcert-ethereum-asset-ledger/src/tests/core/ledger/approve-account-instance-method.test.ts
+++ packages/0xcert-ethereum-asset-ledger/src/tests/core/ledger/approve-account-instance-method.test.ts
@@ -1,11 +1,13 @@
 import { Spec } from '@specron/spec';
 import { GenericProvider } from '@0xcert/ethereum-generic-provider';
+import { OrderGateway } from '@0xcert/ethereum-order-gateway';
 import { Protocol } from '@0xcert/ethereum-sandbox';
 import { AssetLedger } from '../../../core/ledger';
 
 interface Data {
   provider: GenericProvider;
   ledger: AssetLedger;
+  gateway: OrderGateway;
   protocol: Protocol;
   bob: string;
   coinbase: string;
@@ -15,6 +17,7 @@ const spec = new Spec<Data>();
 
 spec.before(async (stage) => {
   const protocol = new Protocol(stage.web3);
+  
   stage.set('protocol', await protocol.deploy());
 });
 
@@ -28,28 +31,48 @@ spec.before(async (stage) => {
 });
 
 spec.before(async (stage) => {
-  const provider = stage.get('provider');
-  const ledgerId = stage.get('protocol').xcert.instance.options.address;
-
-  stage.set('ledger', new AssetLedger(provider, ledgerId));
-});
-
-spec.before(async (stage) => {
   const accounts = await stage.web3.eth.getAccounts();
+
   stage.set('coinbase', accounts[0]);
   stage.set('bob', accounts[1]);
 });
 
-spec.test('approve account for token transfer', async (ctx) => {
+spec.before(async (stage) => {
+  const provider = stage.get('provider');
+  const ledgerId = stage.get('protocol').xcert.instance.options.address;
+  const orderGatewayId = stage.get('protocol').orderGateway.instance.options.address;
+
+  stage.set('ledger', new AssetLedger(provider, ledgerId));
+  stage.set('gateway', new OrderGateway(provider, orderGatewayId));
+});
+
+spec.before(async (stage) => {
+  const xcert = stage.get('protocol').xcert;
+  const coinbase = stage.get('coinbase');
+
+  await xcert.instance.methods.mint(coinbase, '1', '0x973124ffc4a03e66d6a4458e587d5d6146f71fc57f359c8d516e0b12a50ab0d9').send({ from: coinbase });
+  await xcert.instance.methods.mint(coinbase, '2', '0x973124ffc4a03e66d6a4458e587d5d6146f71fc57f359c8d516e0b12a50ab0d9').send({ from: coinbase });
+});
+
+spec.test('approves account for token transfer', async (ctx) => {
+  const xcert = ctx.get('protocol').xcert;
+  const bob = ctx.get('bob');
+  const ledger = ctx.get('ledger');
+
+  await ledger.approveAccount(bob, '1');
+
+  ctx.is(await xcert.instance.methods.getApproved('1').call(), bob);
+});
+
+spec.test('approves order gateway proxy for token transfer', async (ctx) => {
   const xcert = ctx.get('protocol').xcert;
   const ledger = ctx.get('ledger');
-  const coinbase = ctx.get('coinbase');
-  const bob = ctx.get('bob');
-  
-  await xcert.instance.methods.mint(coinbase, '1', '0x973124ffc4a03e66d6a4458e587d5d6146f71fc57f359c8d516e0b12a50ab0d9').send({ from: coinbase });
-  await ledger.approveAccount(bob, '1');
-  const approvedAccount = await xcert.instance.methods.getApproved('1').call();
-  ctx.is(approvedAccount, bob);
+  const gateway = ctx.get('gateway');
+  const proxyId = ctx.get('protocol').nftokenSafeTransferProxy.instance.options.address;
+
+  await ledger.approveAccount(gateway, '2');
+
+  ctx.is(await xcert.instance.methods.getApproved('2').call(), proxyId);
 });
 
 export default spec;
