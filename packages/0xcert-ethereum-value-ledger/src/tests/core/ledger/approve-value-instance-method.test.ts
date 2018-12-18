95c07e55d19e9fabeb813503d70975e996ff0b89
Kristijan Sedlak <xpepermint@gmail.com>
2018-12-18 21:10:57 +0100

    Add value ledger docs

--- packages/0xcert-ethereum-value-ledger/src/tests/core/ledger/approve-value-instance-method.test.ts
+++ packages/0xcert-ethereum-value-ledger/src/tests/core/ledger/approve-value-instance-method.test.ts
@@ -46,7 +46,7 @@ spec.test('approves account for value transfer', async (ctx) => {
   const bob = ctx.get('bob');
   const token = ctx.get('protocol').erc20;
   const value = '300000000000000000000000'; 
-  await ledger.approveValue(bob, value);
+  await ledger.approveValue(value, bob);
   ctx.is(await token.instance.methods.allowance(coinbase, bob).call(), value);
 });
 
@@ -57,7 +57,7 @@ spec.test('approves order gateway proxy for value transfer', async (ctx) => {
   const proxyId = ctx.get('protocol').tokenTransferProxy.instance.options.address;
   const token = ctx.get('protocol').erc20;
   const value = '300000000000000000000000'; 
-  await ledger.approveValue(gateway, value);
+  await ledger.approveValue(value, gateway);
   ctx.is(await token.instance.methods.allowance(coinbase, proxyId).call(), value);
 });
 
