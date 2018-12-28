6a0207bbc8c54708c1e4ab93bc1e93caf2c9fde9
davidlicen <david@artnetik.si>
2018-12-28 11:08:36 +0100

    Update documentation deployment

--- docs/.vuepress/deploy.sh
+++ docs/.vuepress/deploy.sh
@@ -1,10 +1,11 @@
 
 set -e
-vuepress build
-cd dist
+cd ../..
+vuepress build docs
+cd docs/.vuepress/dist
 echo 'docs.0xcert.org' > CNAME
 git init
 git add -A
-git commit -m 'Deploy documentation'
+git commit -m "Deploy documentation"
 git push -f git@github.com:0xcert/docs master
 cd -
