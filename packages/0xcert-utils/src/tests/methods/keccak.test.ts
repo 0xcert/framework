83228d472046020b8748053ce71815cceeabb506
Tadej Vengust <vengust.tadej@gmail.com>
2019-01-04 11:45:55 +0100

    Update keccak implementation.

--- packages/0xcert-utils/src/tests/methods/keccak.test.ts
+++ packages/0xcert-utils/src/tests/methods/keccak.test.ts
@@ -1,11 +1,11 @@
 import { Spec } from '@specron/spec';
-import { keccak } from '../../methods/keccak';
+import { keccak256 } from '../../methods/keccak';
 
 const spec = new Spec();
 
-spec.test('converts string into SHA3 hash', async (ctx) => {
-  ctx.is(await keccak(256, 'foo#'), 'e7bb87eda991af44cdb673a7d5f2bfeb854dd089e421b3934d022fc5c29259a0');
-  ctx.is(await keccak(256, 'bar!'), '179eb773d34690d312adc3f96661402e2de2009e39dbf4b8dee22b3adc842752');
+spec.test('converts string into keccak256 hash', async (ctx) => {
+  ctx.is(keccak256('foo#'), '0xe7bb87eda991af44cdb673a7d5f2bfeb854dd089e421b3934d022fc5c29259a0');
+  ctx.is(keccak256('bar!'), '0x179eb773d34690d312adc3f96661402e2de2009e39dbf4b8dee22b3adc842752');
 });
 
 export default spec;
