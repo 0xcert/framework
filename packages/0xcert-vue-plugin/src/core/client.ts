c549e7a900a5190b57a4c80828d118b4fa5de5be
Kristijan Sedlak <xpepermint@gmail.com>
2019-01-24 14:43:14 +0100

    Refactor vue plugin

--- packages/0xcert-vue-plugin/src/core/client.ts
+++ packages/0xcert-vue-plugin/src/core/client.ts
@@ -1,12 +1,12 @@
 /**
- * 
+ *
  */
 export class Client {
   public provider;
   public modules;
 
   /**
-   * 
+   *
    */
   public constructor(options: {
     provider: any;
@@ -17,70 +17,66 @@ export class Client {
   }
 
   /**
-   * 
+   *
    */
   public getAssetLedger(id: string) {
     const module = this.getModule('AssetLedger');
 
     if (!module) {
       throw this.getMissingModuleError('AssetLedger');
-    }
-    else {
+    } else {
       return new module.object(this.provider, id);
     }
   }
 
   /**
-   * 
+   *
    */
   public getOrderGateway(id?: string) {
     const module = this.getModule('OrderGateway');
 
     if (!module) {
       throw this.getMissingModuleError('OrderGateway');
-    }
-    else {
+    } else {
       return new module.object(this.provider, id);
     }
   }
 
   /**
-   * 
+   *
    */
   public getValueLedger(id: string) {
     const module = this.getModule('ValueLedger');
 
     if (!module) {
       throw this.getMissingModuleError('ValueLedger');
-    }
-    else {
+    } else {
       return new module.object(this.provider, id);
     }
   }
 
   /**
-   * 
+   *
    */
   public createCert(schema: any) {
     const module = this.getModule('Cert');
 
     if (!module) {
       throw this.getMissingModuleError('Cert');
-    }
-    else {
+    } else {
       return new module.object(schema);
     }
   }
 
   /**
-   * 
+   *
    */
   protected getModule(name: string) {
     return this.modules.find((m) => m.name === name);
   }
 
   /**
-   * 
+   *
    */
   protected getMissingModuleError(name: string) {
     throw new Error(`The ${name} module is not available.`);
--- packages/0xcert-vue-plugin/src/core/plugin.ts
+++ packages/0xcert-vue-plugin/src/core/plugin.ts
@@ -1,17 +1,17 @@
-import { Client } from './client'
+import { Client } from './client';
 
 /**
- * 
+ *
  */
 export const Vue0xcert = {
   install(Vue, options) {
     Vue.prototype.$0xcert = new Client(options);
-  }
-}
+  },
+};
 
-/*
-* The plugin is automatically installed when loaded in browser (not as module).
-*/
+/**
+ * The plugin is automatically installed when loaded in browser (not as module).
+ */
 if (typeof window !== 'undefined' && !!window['Vue']) {
   window['Vue'].use(Vue0xcert);
 }
