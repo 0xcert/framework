c47a165f58fca153611aab93dcaf293f8e016b0f
William Entriken <github.com@phor.net>
2019-01-29 23:58:37 -0500

    Use consistent wording with standard

--- packages/0xcert-ethereum-utils-contracts/src/contracts/utils/erc165.sol
+++ packages/0xcert-ethereum-utils-contracts/src/contracts/utils/erc165.sol
@@ -7,7 +7,7 @@ interface ERC165
 {
 
   /**
-   * @dev Checks if the smart contract includes a specific interface.
+   * @dev Checks if the smart contract implements a specific interface.
    * @notice This function uses less than 30,000 gas.
    * @param _interfaceID The interface identifier, as specified in ERC-165.
    */
