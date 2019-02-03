d0557a489792cb48c81202276b6a43b0e730b4c0
William Entriken <github.com@phor.net>
2019-02-02 19:48:16 -0500

    Use description from convention as documentation for TypeScript class

--- packages/0xcert-conventions/src/assets/erc721.ts
+++ packages/0xcert-conventions/src/assets/erc721.ts
@@ -2,9 +2,15 @@
  * ERC-721 unique asset data object interface.
  */
 export interface ObjectErc721 {
+  /** A detailed description of an asset */
   description?: string;
+
+  /** A valid URI pointing to a resource with mime type image/* representing the asset to which this NFT represents. Consider making any images at a width between 320 and 1080 pixels and aspect ratio between 1.91:1 and 4:5 inclusive. */
   image?: string;
+
+  /** A name of an asset */
   name?: string;
+
   [key: string]: any;
 }
 
