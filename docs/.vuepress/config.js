88a2c813eb636fc1ffd4ca1efb0a4061c4da069d
davidlicen <david@artnetik.si>
2019-02-22 11:52:56 +0100

    Put back docs config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,80 @@
+module.exports = {
+  title: '0xcert Docs',
+  description: 'Create, own, and validate unique assets on the blockchain. 0xcert is the first open protocol built to support the future of digital assets, powered by the non-fungible tokens.',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }],
+  ],
+  ga: 'UA-114983924-2',
+  serviceWorker: true,
+  themeConfig: {
+    logo: '/0xcert-logo.svg',
+    displayAllHeaders: false,
+    lastUpdated: 'Last Updated', // string | boolean
+    repo: '0xcert/framework',
+    repoLabel: 'GitHub',
+    docsRepo: '0xcert/framework',
+    docsDir: 'docs',
+    docsBranch: 'master',
+    editLinks: true,
+    editLinkText: 'Help us improve this page.',
+    serviceWorker: {
+      updatePopup: true,
+    },
+    nav: [
+      { text: '0xcert Home', link: 'https://0xcert.org' },
+    ],
+    sidebar: [
+      {
+        title: 'Guide',
+        collapsable: false,
+        children: [
+          ['/guide/introduction', 'Introduction'],
+          ['/guide/getting-started', 'Getting started'],
+          ['/guide/communication', 'Communication'],
+          ['/guide/using-providers', 'Using providers'],
+          ['/guide/about-assets', 'About assets'],
+          ['/guide/certification', 'Certification'],
+          ['/guide/asset-management', 'Asset management'],
+          ['/guide/about-cryptocurrency', 'About cryptocurrency'],
+          ['/guide/value-management', 'Value management'],
+          ['/guide/atomic-orders', 'Atomic orders'],
+        ],
+      },
+      {
+        title: 'API',
+        collapsable: false,
+        children: [
+          ['/api/core', 'Core'],
+          ['/api/ethereum', 'Ethereum'],
+        ],
+      },
+      {
+        title: 'Plugins',
+        collapsable: false,
+        children: [
+          ['/plugins/vuejs', 'VueJS'],
+        ],
+      },
+    ],
+  },
+  markdown: {
+    lineNumbers: false,
+    config(md) {
+      md.use(require('markdown-it-container'), 'card', {
+        validate: function (params) {
+          return params.trim().match(/^card\s+(.*)$/);
+        },
+        render: function (tokens, idx) {
+          let title = tokens[idx].info.trim().match(/^card\s+(.*)$/);
+          return tokens[idx].nesting === 1 ? 
+            '<div class="card custom-block"><p class="custom-block-title">' + md.utils.escapeHtml(title[1]) + "</p>\n"
+            : '</div>\n';
+        },
+      });
+    },
+    externalLinks: {
+      target: '_self',
+      rel: 'noopener noreferrer alternate',
+    },
+  },
+};
