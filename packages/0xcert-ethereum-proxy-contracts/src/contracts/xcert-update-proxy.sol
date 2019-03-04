6f1df820d052a86b963a79a7aca920bff5364366
Tadej Vengust <vengust.tadej@gmail.com>
2019-03-04 15:37:01 +0100

    Add update proxy.

---
+++ packages/0xcert-ethereum-proxy-contracts/src/contracts/xcert-update-proxy.sol
@@ -0,0 +1,37 @@
+pragma solidity 0.5.1;
+
+import "@0xcert/ethereum-xcert-contracts/src/contracts/ixcert-mutable.sol";
+import "@0xcert/ethereum-utils-contracts/src/contracts/permission/abilitable.sol";
+
+/**
+ * @title XcertUpdateProxy - updates a token on behalf of contracts that have been approved via
+ * decentralized governance.
+ */
+contract XcertUpdateProxy is 
+  Abilitable 
+{
+
+  /**
+   * @dev List of abilities:
+   * 2 - Ability to execute create. 
+   */
+  uint8 constant ABILITY_TO_EXECUTE = 2;
+
+  /**
+   * @dev Updates imprint of an existing Xcert.
+   * @param _xcert Address of the Xcert contract on which the update will be perfomed.
+   * @param _id The Xcert we will update.
+   * @param _imprint Cryptographic asset imprint.
+   */
+  function update(
+    address _xcert,
+    uint256 _id,
+    bytes32 _imprint
+  )
+    external
+    hasAbilities(ABILITY_TO_EXECUTE)
+  {
+    XcertMutable(_xcert).updateTokenImprint(_id, _imprint);
+  }
+  
+}
