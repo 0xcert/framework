fe90bd05a218b1cebabbb3deb1721b178b2c0391
Tadej Vengust <vengust.tadej@gmail.com>
2019-03-20 11:41:34 +0100

    Update solidity version.

--- packages/0xcert-ethereum-proxy-contracts/src/contracts/xcert-update-proxy.sol
+++ packages/0xcert-ethereum-proxy-contracts/src/contracts/xcert-update-proxy.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.1;
+pragma solidity 0.5.6;
 
 import "@0xcert/ethereum-xcert-contracts/src/contracts/ixcert-mutable.sol";
 import "@0xcert/ethereum-utils-contracts/src/contracts/permission/abilitable.sol";
