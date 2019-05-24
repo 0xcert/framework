6a7b86b48a49cbf577daae369c9a5c3861ece00a
Tadej Vengust <vengust.tadej@gmail.com>
2019-05-24 08:53:46 +0200

    Add sandbox to provider options.

--- packages/0xcert-ethereum-metamask-provider/src/core/provider.ts
+++ packages/0xcert-ethereum-metamask-provider/src/core/provider.ts
@@ -39,6 +39,11 @@ export interface MetamaskProviderOptions {
    * The number of milliseconds in which a mutation times out.
    */
   mutationTimeout?: number;
+
+  /**
+   * Sandbox mode. False by default.
+   */
+  sandbox?: Boolean;
 }
 
 /**
