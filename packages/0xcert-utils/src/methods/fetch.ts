e6b8c77980b259cc1f9945c5b9a63a28331818af
Tadej Vengust <vengust.tadej@gmail.com>
2019-07-31 15:59:51 +0200

    Update fetch methods with local reading.

--- packages/0xcert-utils/src/methods/fetch.ts
+++ packages/0xcert-utils/src/methods/fetch.ts
@@ -3,7 +3,16 @@
  * @param path URL or local path.
  */
 export async function fetch(path, options?) {
-  if (typeof window !== 'undefined') {
+  if (path.lastIndexOf('http', 0) !== 0) {
+    return new Promise((resolve, reject) => {
+      require('fs').readFile(path, 'utf8', function(err, data) {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+    });
+  } else if (typeof window !== 'undefined') {
     return (window as any).fetch(path, options);
   } else {
     return require('node-fetch')(path, options);
--- packages/0xcert-utils/src/tests/methods/fetch.test.ts
+++ packages/0xcert-utils/src/tests/methods/fetch.test.ts
@@ -1,10 +1,16 @@
 import { Spec } from '@hayspec/spec';
+import * as path from 'path';
 import { fetch } from '../../methods/fetch';
 
 const spec = new Spec();
 
 spec.test('downloads a remote file', async (ctx) => {
   const res = await fetch('https://docs.0xcert.org/xcert-mock.json').then((r) => r.json());
+  ctx.true(!!res.XcertMock);
+});
+
+spec.test('reads a local file', async (ctx) => {
+  const res = await fetch(path.join(__dirname, '..', 'mocks', 'xcert-mock.json')).then((r) => JSON.parse(r));
   ctx.true(!!res.XcertMock);
 });
 
