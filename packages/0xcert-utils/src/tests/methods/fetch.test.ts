c80510ec913985cff31c41306b11874272a66daf
Tadej Vengust <vengust.tadej@gmail.com>
2019-08-01 10:50:23 +0200

    Refactor fetch to always return JSON.

--- packages/0xcert-utils/src/tests/methods/fetch.test.ts
+++ packages/0xcert-utils/src/tests/methods/fetch.test.ts
@@ -1,16 +1,16 @@
 import { Spec } from '@hayspec/spec';
 import * as path from 'path';
-import { fetch } from '../../methods/fetch';
+import { fetchJson } from '../../methods/fetch';
 
 const spec = new Spec();
 
 spec.test('downloads a remote file', async (ctx) => {
-  const res = await fetch('https://docs.0xcert.org/xcert-mock.json').then((r) => r.json());
+  const res = await fetchJson('https://docs.0xcert.org/xcert-mock.json');
   ctx.true(!!res.XcertMock);
 });
 
 spec.test('reads a local file', async (ctx) => {
-  const res = await fetch(path.join(__dirname, '..', 'mocks', 'xcert-mock.json')).then((r) => JSON.parse(r));
+  const res = await fetchJson(path.join(__dirname, '..', 'mocks', 'xcert-mock.json'));
   ctx.true(!!res.XcertMock);
 });
 
