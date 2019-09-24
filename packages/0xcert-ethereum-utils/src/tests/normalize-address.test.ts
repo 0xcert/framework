4281430c356991dd773d493fd66fb68a52ec4383
Tadej Vengust <vengust.tadej@gmail.com>
2019-09-24 17:23:02 +0200

    Make checksum normalization more robust.

--- packages/0xcert-ethereum-utils/src/tests/normalize-address.test.ts
+++ packages/0xcert-ethereum-utils/src/tests/normalize-address.test.ts
@@ -8,6 +8,7 @@ spec.test('normalize address', (ctx) => {
   ctx.is(normalizeAddress('0x85a9916425960aa35b2a527d77c71855dc0215b3'), '0x85A9916425960aA35B2a527D77C71855DC0215B3');
   ctx.is(normalizeAddress('0x85A9916425960AA35B2A527D77C71855DC0215B3'), '0x85A9916425960aA35B2a527D77C71855DC0215B3');
   ctx.is(normalizeAddress('85A9916425960aA35B2a527D77C71855DC0215B3'), '0x85A9916425960aA35B2a527D77C71855DC0215B3');
+  ctx.is(normalizeAddress('0x85A9916425960aA35B2a527D77C71855DC0215b3'), '0x85A9916425960aA35B2a527D77C71855DC0215B3');
   ctx.is(normalizeAddress(null), null);
   ctx.throws(() => normalizeAddress('dfg'));
 });
