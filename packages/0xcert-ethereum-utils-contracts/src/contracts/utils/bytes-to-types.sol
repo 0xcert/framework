61424c1280ac5c2f7a0bb9a8298f233bfc948a46
Tadej Vengust <vengust.tadej@gmail.com>
2019-09-27 11:33:11 +0200

    Unpack bytes to solidity types.

---
+++ packages/0xcert-ethereum-utils-contracts/src/contracts/utils/bytes-to-types.sol
@@ -0,0 +1,112 @@
+pragma solidity 0.5.6;
+
+/**
+ * @title BytesToTypes
+ * @dev The BytesToTypes contract converts the memory byte arrays to the standard solidity types.
+ * @notice Based on: https://github.com/pouladzade/Seriality/blob/master/src/BytesToTypes.sol
+ */
+contract BytesToTypes
+{
+  /**
+   * @dev Converts bytes to address type.
+   * @param _offst Offset from where the conversion starts.
+   * @param _input Bytes to convert.
+   */
+  function bytesToAddress(
+    uint _offst,
+    bytes memory _input
+  )
+    internal
+    pure
+  returns (address _output)
+  {
+    assembly { _output := mload(add(_input, _offst)) } // solhint-disable-line
+  }
+
+  /**
+   * @dev Converts bytes to bool type.
+   * @param _offst Offset from where the conversion starts.
+   * @param _input Bytes to convert.
+   */
+  function bytesToBool(
+    uint _offst,
+    bytes memory _input
+  )
+    internal
+    pure
+    returns (bool _output)
+  {
+    uint8 x;
+    assembly { x := mload(add(_input, _offst)) } // solhint-disable-line
+    if (x == 0) {
+      _output = false;
+    } else {
+      _output = true;
+    }
+  }
+
+  /**
+   * @dev Converts bytes to bytes32 type.
+   * @param _offst Offset from where the conversion starts.
+   * @param _input Bytes to convert.
+   * @param _output Bytes32 output.
+   */
+  function bytesToBytes32(
+    uint _offst,
+    bytes memory _input
+  )
+    internal
+    pure
+    returns (bytes32 _output)
+  {
+    assembly { _output := mload(add(_input, _offst)) } // solhint-disable-line
+  }
+
+  /**
+   * @dev Converts bytes to uint8 type.
+   * @param _offst Offset from where the conversion starts.
+   * @param _input Bytes to convert.
+   */
+  function bytesToUint8(
+    uint _offst,
+    bytes memory _input
+  )
+    internal
+    pure
+    returns (uint8 _output)
+  {
+    assembly { _output := mload(add(_input, _offst)) } // solhint-disable-line
+  }
+
+  /**
+   * @dev Converts bytes to uint16 type.
+   * @param _offst Offset from where the conversion starts.
+   * @param _input Bytes to convert.
+   */
+  function bytesToUint16(
+    uint _offst,
+    bytes memory _input
+  )
+    internal
+    pure
+    returns (uint16 _output)
+  {
+    assembly { _output := mload(add(_input, _offst)) } // solhint-disable-line
+  }
+
+  /**
+   * @dev Converts bytes to uint256 type.
+   * @param _offst Offset from where the conversion starts.
+   * @param _input Bytes to convert.
+   */
+  function bytesToUint256(
+    uint _offst,
+    bytes memory _input
+  )
+    internal
+    pure
+    returns (uint256 _output)
+  {
+    assembly { _output := mload(add(_input, _offst)) } // solhint-disable-line
+  }
+}
---
+++ packages/0xcert-ethereum-utils-contracts/src/tests/mocks/unpack-test-mock.sol
@@ -0,0 +1,30 @@
+pragma solidity 0.5.6;
+
+import "../../contracts/utils/bytes-to-types.sol";
+
+contract UnpackTestMock is
+  BytesToTypes
+{
+
+  function unpack(
+    bytes calldata _input
+  )
+    external
+    pure
+    returns (address p1, bool p2, bytes32 p3, uint8 p4, uint16 p5, uint256 p6)
+  {
+    uint8 offset = 88;
+    p1 = bytesToAddress(offset, _input);
+    offset -= 20;
+    p2 = bytesToBool(offset, _input);
+    offset -= 1;
+    p3 = bytesToBytes32(offset, _input);
+    offset -= 32;
+    p4 = bytesToUint8(offset, _input);
+    offset -= 1;
+    p5 = bytesToUint16(offset, _input);
+    offset -= 2;
+    p6 = bytesToUint256(offset, _input);
+  }
+
+}
