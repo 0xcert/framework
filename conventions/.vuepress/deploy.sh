0f0e7db865f785c4a226349f48bd9a7a1e5f8e1a
Tadej Vengust <vengust.tadej@gmail.com>
2019-11-28 13:02:56 +0100

    Fix conventions deploy script to deploy ethereum and wanchain contracts.

--- conventions/.vuepress/deploy.sh
+++ conventions/.vuepress/deploy.sh
@@ -1,5 +1,12 @@
-cp ../../packages/0xcert-ethereum-erc20-contracts/build/token-mock.json ./public
-cp ../../packages/0xcert-ethereum-xcert-contracts/build/xcert-mock.json ./public
+cp ../../packages/0xcert-ethereum-erc20-contracts/build/token-mock.json ./public/ethereum/
+cp ../../packages/0xcert-ethereum-xcert-contracts/build/xcert-mock.json ./public/ethereum/
+cd ../../packages/0xcert-ethereum-erc20-contracts
+npx specron compile --evmVersion byzantium --build ./build/wanchain
+cd ../0xcert-ethereum-xcert-contracts
+npx specron compile --evmVersion byzantium --build ./build/wanchain
+cd ../../conventions/.vuepress
+cp ../../packages/0xcert-ethereum-erc20-contracts/build/wanchain/token-mock.json ./public/wanchain/
+cp ../../packages/0xcert-ethereum-xcert-contracts/build/wanchain/xcert-mock.json ./public/wanchain/
 set -e
 npm run build
 cd dist
