cf4a8ed22bd758cf69fdbe17391ab47fdc54c4ae
Kristijan Sedlak <xpepermint@gmail.com>
2019-12-10 15:00:12 +0100

    Add missing conventions exports

--- packages/0xcert-conventions/src/index.ts
+++ packages/0xcert-conventions/src/index.ts
@@ -2,3 +2,4 @@ export * from './assets/86-base-asset';
 export * from './assets/87-asset-evidence';
 export * from './assets/88-crypto-collectible';
 export * from './assets/erc721';
+export * from './assets/xcert-schema';
