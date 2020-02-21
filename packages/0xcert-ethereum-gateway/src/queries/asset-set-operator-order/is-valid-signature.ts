f17b1e77e997f0d7f6486a55df029c9085ad6bed
Tadej Vengust <vengust.tadej@gmail.com>
2020-02-21 14:07:38 +0100

    Fix valid signature check method.

--- packages/0xcert-ethereum-gateway/src/queries/asset-set-operator-order/is-valid-signature.ts
+++ packages/0xcert-ethereum-gateway/src/queries/asset-set-operator-order/is-valid-signature.ts
@@ -1,9 +1,8 @@
 import { AssetSetOperatorOrder } from '@0xcert/scaffold';
 import { Gateway } from '../../core/gateway';
-import { createSignatureTuple, getOrderInputParams } from '../../lib/asset-set-operator-order';
-
+import { createOrderHash, createSignatureTuple } from '../../lib/asset-set-operator-order';
 const functionSignature = '0x8fa76d8d';
-const inputTypes = ['address', 'address', 'bool', 'address', 'uint256', 'address', 'uint256', 'uint256', 'tuple(bytes32, bytes32, uint8, uint8)'];
+const inputTypes = ['address', 'bytes32', 'tuple(bytes32, bytes32, uint8, uint8)'];
 const outputTypes = ['bool'];
 
 /**
@@ -14,12 +13,11 @@ const outputTypes = ['bool'];
  */
 export default async function(gateway: Gateway, order: AssetSetOperatorOrder, claim: string) {
   const signatureTuple = createSignatureTuple(claim);
-  const params = getOrderInputParams(order);
-  params.push(signatureTuple);
+  const orderHash = createOrderHash(order);
   try {
     const attrs = {
       to: order.ledgerId,
-      data: functionSignature + gateway.provider.encoder.encodeParameters(inputTypes, params).substr(2),
+      data: functionSignature + gateway.provider.encoder.encodeParameters(inputTypes, [order.owner, orderHash, signatureTuple]).substr(2),
     };
     const res = await gateway.provider.post({
       method: 'eth_call',
