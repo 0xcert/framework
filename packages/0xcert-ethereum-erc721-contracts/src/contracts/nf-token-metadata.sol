8c274b8d0f28a9e2816121f7d1712a8bb19e1e61
Tadej Vengust <vengust.tadej@gmail.com>
2021-01-14 09:13:02 +0100

    Fix metadata uint2str function.

--- packages/0xcert-ethereum-erc721-contracts/src/contracts/nf-token-metadata.sol
+++ packages/0xcert-ethereum-erc721-contracts/src/contracts/nf-token-metadata.sol
@@ -486,11 +486,11 @@ contract NFTokenMetadata is
       j /= 10;
     }
     bytes memory bstr = new bytes(length);
-    uint256 k = length - 1;
+    uint256 k = length;
     j = _i;
     while (j != 0)
     {
-      bstr[k--] = bytes1(uint8(48 + j % 10));
+      bstr[--k] = bytes1(uint8(48 + j % 10));
       j /= 10;
     }
     str = string(bstr);
